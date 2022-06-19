dfac8928fad83d3fb85bba421474124a2e0c1753
Mahmoud Magdy <moudmagdy@hotmail.com>
2022-06-19 09:00:19 +0200

    ui fixes

--- js/components/side-panel.module.js
+++ js/components/side-panel.module.js
@@ -1,54 +1,18 @@
-const sidePanelBtns = document.querySelectorAll('.side-panel--btn');
-const dismissBtns = document.querySelectorAll('.side-panel__dismiss--btn');
+const sidePanelTriggers = document.querySelectorAll('[data-panel-target]');
+const dismissBtns = document.querySelectorAll('[data-dismiss="panel"]');
 
-
-
-if (sidePanelBtns) {
-    sidePanelBtns.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            e.preventDefault();
-            let btnDataAttr = btn.getAttribute('data-panel');
-            document.querySelector('body').classList.add('side-panel--shown');
-            document.querySelector('.side-panel[data-panel="' + btnDataAttr + '"]').classList.add('move-panel');
-        });
-    });
-}
-
-if (dismissBtns) {
-    dismissBtns.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            e.preventDefault();
-            document.querySelector('body').classList.remove('side-panel--shown');
-            btn.closest('.side-panel').classList.remove('move-panel');
-        });
-    });
-}
-
-// Control panel filters
-const contentFilter = document.querySelectorAll('.content__filter__item');
-const invoiceList = document.querySelector('.invoice__list');
-function loadFilteredContent() {
-    let selectedFilterDataAttr = document.querySelector('.selected--filter').getAttribute('data-filter');
-
-    invoiceList.querySelectorAll('.invoice__item[data-filter="' + selectedFilterDataAttr + '"]').forEach(item => {
-        item.classList.add('item--shown');
-    });
-}
-function filterContent(e) {
+function openPanel(e) {
     e.preventDefault();
-
-    let filterDataAttr = this.getAttribute('data-filter');
-
-    document.querySelector('.content__filter__item.selected--filter').classList.remove('selected--filter');
-    this.classList.add('selected--filter');
-
-    invoiceList.querySelectorAll('.invoice__item').forEach(item => {
-        item.classList.remove('item--shown');
-    });
-
-    invoiceList.querySelectorAll('.invoice__item[data-filter="' + filterDataAttr + '"]').forEach(item => {
-        item.classList.add('item--shown');
-    });
+    let triggerDataAttr = this.getAttribute('data-panel-target');
+    document.querySelector('body').classList.add('side-panel--shown');
+    document.querySelector('.side-panel[data-panel="' + triggerDataAttr + '"]').classList.add('move-panel');
 }
-window.addEventListener('load', loadFilteredContent);
-contentFilter.forEach(filter => filter.addEventListener('click', filterContent));
+
+function closePanel(e) {
+    e.preventDefault();
+    document.querySelector('body').classList.remove('side-panel--shown');
+    this.closest('.side-panel').classList.remove('move-panel');
+}
+
+sidePanelTriggers ? sidePanelTriggers.forEach(trigger => trigger.addEventListener('click', openPanel)) : '';
+dismissBtns ? dismissBtns.forEach(btn => btn.addEventListener('click', closePanel)) : '';
